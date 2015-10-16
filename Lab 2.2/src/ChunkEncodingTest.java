cf7596e7004f67d87bb70d1f04d650c468e37cb2
Osi2 <Yegor.Osipov@gmail.com>
2015-10-16 17:25:33 +0300

    3-rd commit

--- Lab 2.2/src/ChunkEncodingTest.java
+++ Lab 2.2/src/ChunkEncodingTest.java
@@ -11,16 +11,19 @@ public class ChunkEncodingTest {
 
     public static void main(String[] args) throws IOException {
 
-        ChunkEncoderInputStream is = new ChunkEncoderInputStream(new FileInputStream(fileIn), 10);
-        ChunkDecoderOutputStream os = new ChunkDecoderOutputStream(new FileOutputStream(fileOut));
+        try( ChunkEncoderInputStream is = new ChunkEncoderInputStream(new FileInputStream(fileIn), 10);
+             ChunkDecoderOutputStream os = new ChunkDecoderOutputStream(new FileOutputStream(fileOut))){
 
-        while (!is.IsEmpty()){
-            byte[] encodedChunk = is.encodeNextPortion();
-            String encodedString = new String(encodedChunk,"UTF-8");
-            System.out.println("encoded string: " + encodedString);
-            System.out.println("encoded string: [" + encodedString.replaceAll("\r","\\\\r").replaceAll("\n","\\\\n") + "]");
-            os.write(encodedChunk);
-        }
+
+            while (!is.IsEmpty()) {
+                byte[] encodedChunk = is.encodeNextPortion();
+                String encodedString = new String(encodedChunk, "UTF-8");
+                System.out.println("encoded string: " + encodedString);
+                System.out.println("encoded string: [" + encodedString.replaceAll("\r", "\\\\r").replaceAll("\n", "\\\\n") + "]");
+                os.write(encodedChunk);
+            }
+
+        }catch (IOException ex){ex.printStackTrace();}
 
 
 
