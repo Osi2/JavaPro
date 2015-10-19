fda57f592ee5671e45b3029aa0e2d0701b367d2a
Osi2 <Yegor.Osipov@gmail.com>
2015-10-19 10:09:44 +0300

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	Lab 2.2/src/ChunkEncoderInputStream.java
    	Lab 2.2/src/ChunkEncodingTest.java

--- Lab 2.3/src/Client.java
+++ Lab 2.3/src/Client.java
@@ -1,9 +1,15 @@
 import java.lang.Exception;
 import java.io.*;
 import java.net.*;
+import java.nio.ByteBuffer;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.StringTokenizer;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.apache.commons.codec.binary.*;
 
 public class Client implements Runnable {
     private Socket socket;
@@ -44,14 +50,72 @@ public class Client implements Runnable {
         return res.toString().getBytes();
     }
 
+    private void process3(InputStream is, OutputStream os) throws IOException {
+
+            DataOutputStream out = new DataOutputStream(new FileOutputStream("D:/Temp/_Work/fos.bin"));
+            String s = patternSearch(is);
+            out.write(s.getBytes());
+            /*
+
+            DataOutputStream out = new DataOutputStream(new FileOutputStream("D:/Temp/_Work/fos.bin"));
+
+            int byteRead;
+            // Read a raw byte, returns an int of 0 to 255.
+            while ((byteRead = is.read()) != -1) {
+                // Write the least-significant byte of int, drop the upper 3 bytes
+                out.write(byteRead);
+            }
+            */
+        out.flush();
+        out.close();
+
+
+    }
+
+    private String patternSearch(InputStream inputStream){
+        //Pattern currently being checked for
+        Pattern name = Pattern.compile("^-*WebKitFormBoundary.*[\\r\\n].*[\\r\\n].*[]\\r\\n]+((.|\\r|\\n?)*)[\\r\\n]{1}.*$");
+
+        Matcher m;
+        //List<String> arrayList = new ArrayList<String>();
+
+        InputStreamReader isr = new InputStreamReader(inputStream);
+        BufferedReader buf = new BufferedReader(isr);
+        StringBuilder sb = new StringBuilder();
+        String str = null;
+        String s = null;
+
+        try {
+            while ((str = buf.readLine()) != null) {
+                sb.append(str);
+                }
+
+            m = name.matcher(sb.toString());
+            while(m.find()){
+                s = m.group();
+            }
+
+            return  s;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+    }
+
+
     private void process2(InputStream is, OutputStream os) throws IOException {
-        inClient = new BufferedReader(new InputStreamReader(is));
+        //InputStreamReader ir = new InputStreamReader(is);
+        //System.out.println("Encoding:" + ir.getEncoding());
+        inClient = new BufferedReader(new InputStreamReader(is,"UTF-8"));
         String currentLine = inClient.readLine();
+        System.out.println("line:" + currentLine);
         StringTokenizer tokenizer = new StringTokenizer(currentLine);
         String httpMethod = tokenizer.nextToken();
         String httpQueryString = tokenizer.nextToken();
         String responseString = null;
         String contentLength;
+        //Charset charset = Charset.forName("UTF-8");
 
         System.out.println("ProcessFile method: " + httpMethod);
         System.out.println("ProcessFile queryString: " + httpQueryString);
@@ -89,6 +153,11 @@ public class Client implements Runnable {
 
         }
         else if(httpMethod.equals("POST")){
+
+            process3(is,os);
+            if (1==1)
+                return;
+
             PrintWriter fout = null;
             FileOutputStream fs;
             String dirname = "D:\\Temp";
@@ -127,27 +196,33 @@ public class Client implements Runnable {
                 inClient.readLine(); //assert(inFromClient.readLine().equals("")) : "Expected line in POST request is "" ";
 
                 fout = new PrintWriter(dirname + "\\" + filename);
-                fs = new FileOutputStream(dirname + "\\" + filename);
+                fs = new  FileOutputStream(dirname + "\\" + filename);
+                Base64OutputStream bs64Out = new Base64OutputStream(new FileOutputStream(dirname + "\\" + filename));
+
                 String prevLine = inClient.readLine();
                 currentLine = inClient.readLine();
 
                 //Here we upload the actual file contents
                 while (true) {
-                    //System.out.println(prevLine);
+                    System.out.println(prevLine);
                     if (currentLine.indexOf("--" + boundary) != -1) {
                         //fout.print(prevLine);
-                        fs.write(prevLine.getBytes());
+                        //fs.write(prevLine.getBytes());
+                        bs64Out.write(prevLine.getBytes());
                         break;
                     }
                     else {
-                        fs.write(prevLine.getBytes());
+                        //fs.write(prevLine.getBytes());
+                        bs64Out.write(prevLine.getBytes());
                         //fout.println(prevLine.getBytes());
                     }
                     prevLine = currentLine;
                     currentLine = inClient.readLine();
                 }
-                fout.flush();
-                fout.close();
+                bs64Out.flush();
+                bs64Out.close();
+                //fout.flush();
+                //fout.close();
 
                 responseString = "<html><body>File " + filename + " has been uploaded </body></html>";
 
